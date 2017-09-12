7399c652f4aba6b81d0532588d6b09d8e2b7d8f0
Tyler Benton <tjbenton21@gmail.com>
2017-09-12 11:47:07 -0400

    removed bug where extra spaces would get added to output

--- index.js
+++ index.js
@@ -12,40 +12,12 @@ exports.decorateTerm = (Term, { React }) => class extends React.Component {
 }
 
 exports.middleware = (store) => (next) => (action) => {
-  let action_type = action.type
-  
-  if('SESSION_PTY_DATA' === action_type){
-    let data = action.data
-
-    const regex = /:\w+:/g
-    let matches = data.match(regex)
-
-    if(matches){
-      matches.forEach(emoji => {
-        let emoji_data = emoji.split(':')[1]
-        
-        let char = emoji
-        if (emoji_data in emojilib.lib) {
-          char = emojilib.lib[emoji_data].char
-        }
-
-        //replace all
-        action.data = action.data.split(emoji).join(char + ' ')
-      })
-    }
+  if([ 'SESSION_ADD_DATA', 'SESSION_PTY_DATA' ].includes(action.type)){
+    action.data = action.data.replace(/:([a-z0-9_+-]+):/ig, (match, emoji) => {
+      // if the emoji exist and has a char then use it, else return the original match
+      return (emojilib.lib[emoji.toLowerCase()] || {}).char || match
+    })
   }
 
   next(action)
 }
-
-/*
-//Debug for ticket on Hyper.js
-exports.middleware = (store) => (next) => (action) => {
-  if('SESSION_PTY_DATA' === action.type){
-    action.data = action.data.split('hello world').join("hi")
-    debugger
-  }
-  
-  next(action)
-}
-*/
